db9939c471178e1478622ba48add49c01b0dda8e
zaydu <zaydulin.vagiz@gmail.com>
2023-12-06 20:30:31 +0400

    #поправил stopwords в чатах

--- src/events/consumers.py
+++ src/events/consumers.py
@@ -17,25 +17,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.event_slug = self.scope["url_route"]["kwargs"]["slug"]
         await self.channel_layer.group_add(self.event_group_name, self.channel_name)
-        asyncio.ensure_future(self.send_event_data_second())
+        await self.channel_layer.group_add(self.event_group_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, code):
         await self.channel_layer.group_discard(self.event_group_name, self.channel_name)
         await super().disconnect(code=code)
-
-    async def get_event_data(self):
-        event = Events.objects.get(slug=self.event_slug)
-        return {"home_score":event.home_score,"away_score":event.away_score}
-
-    async def send_event_data(self,event_data):
-        await self.send(text_data=json.dumps({"type": "event_update","data":event_data}))
-
-    async def send_event_data_second(self):
-        while True:
-            event_data = await self.get_event_data()
-            await self.send_event_data(event_data)
-            await asyncio.sleep(5)
 
 
 
@@ -56,3 +43,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
         event = Events.objects.get(slug=self.event_slug)
         new_comment = Messages.objects.create(user=self.scope["user"], message=text, event=event)
         return new_comment
+
+    async def update_event_data(self, event):
+        event_data = await self.get_event_data()
+        await self.send(text_data=json.dumps({"event_data": event_data}))
+
+    @database_sync_to_async
+    def get_event_data(self):
+        event = Events.objects.get(slug=self.event_slug)
+        event_data = {
+            "home_score": event.home_score,
+            "away_score": event.away_score,
+        }
+        return event_data
