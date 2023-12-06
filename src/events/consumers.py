36192d1a26271810271c0337179c62f5b812f583
jexterone <allertartur@mail.ru>
2023-12-06 21:07:16 +0400

    чатжпт не помог

--- src/events/consumers.py
+++ src/events/consumers.py
@@ -45,7 +45,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = text_data_json["text"]
 
         new_message = await self.create_new_message(message)
-        data = {"user": new_message.user.username, "message": new_message.message}
+        data = {"user": new_message.user.username, "message": new_message.message,"event":new_message.event.home_score,}
         await self.channel_layer.group_send(self.event_group_name, {"type": "new_message", "message": data})
 
     async def new_message(self, event):
