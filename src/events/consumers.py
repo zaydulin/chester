a6d8eb7e4546708feced0ec7ad0addbbe012b991
jexterone <allertartur@mail.ru>
2023-12-06 16:22:37 +0400

    консумер

--- src/events/consumers.py
+++ src/events/consumers.py
@@ -1,6 +1,6 @@
-import json
-from django.contrib.contenttypes.models import ContentType
+# consumers.py
 
+import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from mainapp.models import Messages
@@ -8,16 +8,20 @@ from events.models import Events
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.event_slug = self.scope["url_route"]["kwargs"]["slug"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.event_slug = None
         self.event_group_name = "post_%s" % self.event_slug
 
+    async def connect(self):
+        self.event_slug = self.scope["url_route"]["kwargs"]["slug"]
         await self.channel_layer.group_add(self.event_group_name, self.channel_name)
-
         await self.accept()
 
     async def disconnect(self, code):
-        await self.channel_layer.discard(self.event_group_name, self.channel_name)
+        await self.channel_layer.group_discard(self.event_group_name, self.channel_name)
+        await super().disconnect(code=code)
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -25,16 +29,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = text_data_json["text"]
 
         new_message = await self.create_new_message(message)
-        data = {"author": new_message.author.username, "text": new_message.text}
+        data = {"user": new_message.user.username, "message": new_message.message}
         await self.channel_layer.group_send(self.event_group_name, {"type": "new_message", "message": data})
 
     async def new_message(self, event):
         message = event["message"]
-
         await self.send(text_data=json.dumps({"message": message}))
 
     @database_sync_to_async
     def create_new_message(self, text):
         event = Events.objects.get(slug=self.event_slug)
-        new_comment = Messages.objects.create(author=self.scope["user"], text=text, event=event)
+        new_comment = Messages.objects.create(user=self.scope["user"], message=text, event=event)
         return new_comment
