15ce7aa7e8ce461a2487d328f8e160c0db1602a8
jexterone <allertartur@mail.ru>
2023-12-07 21:05:05 +0400

    кал

--- src/static/docs/js/script.js
+++ src/static/docs/js/script.js
@@ -20,28 +20,17 @@ function clearContent() {
 document.body.addEventListener('htmx:afterRequest', function(event) {
     const hxCustom = event.srcElement.getAttribute("hx-custom");
     if (hxCustom === 'title') {
-        const dataTitle = document.getElementById("main-content").querySelector("main[data-title]");
-        if (dataTitle) {
-            const textTitle = dataTitle.getAttribute("data-title");
-            const metaContent = dataTitle.getAttribute("data-content");
-
-            // Update title tag
-            document.querySelector("head title").innerHTML = textTitle;
-
-            // Update meta tag
-            const metaDescription = document.querySelector("meta[name='description']");
-            if (metaDescription) {
-                metaDescription.setAttribute("content", metaContent);
-            } else {
-                // Create meta tag if it doesn't exist
-                const newMetaTag = document.createElement("meta");
-                newMetaTag.setAttribute("name", "description");
-                newMetaTag.setAttribute("content", metaContent);
-                document.querySelector("head").appendChild(newMetaTag);
-            }
-        }
+      const dataTitle = document.getElementById("main-content").querySelector("main[data-title]");
+      if (dataTitle) {
+        const textTitle = dataTitle.getAttribute("data-title");
+        document.querySelector("head title").innerHTML = textTitle;
+      }
     }
-});
+    const metaContent = event.srcElement.getAttribute("data-content");
+    if (metaContent) {
+      document.querySelector("meta[name='description']").setAttribute("content", metaContent);
+    }
+  });
 const countries = document.querySelectorAll('.events__search-tour + div div');
 
 for (let i = 0; i < countries.length; i++) {
