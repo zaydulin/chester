812b6e58278d5dc8a7cb33408d0d645204f6fcc7
zaydulin <zaydulin.vagiz@gmail.com>
2023-12-10 21:24:37 +0400

    Вернул

--- src/mainapp/forms.py
+++ src/mainapp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ClearableFileInput
-#from captcha.fields import CaptchaField
+from captcha.fields import CaptchaField
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="Ник на сайте", max_length=100, widget=forms.TextInput(attrs={'placeholder': 'username'}))
@@ -20,7 +20,7 @@ class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True, label='Email')  # Добавьте поле для Email
     username = forms.CharField(required=True, label='Имя')  # Добавьте поле для Email
     birthday = forms.DateField(required=False, label='Дата рождения', widget=forms.DateInput(attrs={'id': 'datepicker'}))
-    #captcha = CaptchaField()
+    captcha = CaptchaField()
     class Meta:
         model = User  # Укажите модель пользователя, обычно 'User'
         fields = ('username','captcha', 'email', 'password1', 'password2', 'gender', 'birthday')  # Поля, которые будут отображаться в форме
