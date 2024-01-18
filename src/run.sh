6270bbd76ba3265769f75745d870a74ed0397725
Алексей Шевич <lehashevich@gmail.com>
2024-01-18 21:19:39 +0300

    fix

--- src/run.sh
+++ src/run.sh
@@ -9,7 +9,7 @@ if [[ $DJANGO_DEBUG -eq 0 ]]; then
   echo "Use production settings"
   # Load static files
   python manage.py collectstatic --noinput
-  daphne -b 0.0.0.0 -p 8001 _project.asgi:application &
+  daphne -b 0.0.0.0 -p 8001 _project.asgi:application --application-close-timeout 1 &
   gunicorn -w 4 --env DJANGO_SETTINGS_MODULE=_project.settings _project.wsgi -b 0.0.0.0:8000
 else
   echo "Use development settings"
