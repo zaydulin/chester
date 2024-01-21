20586a050811ffcfe23e8b5965b250d31b665767
jexterone <allertartur@mail.ru>
2024-01-21 19:02:29 +0400

    tasks

--- src/_project/settings/core_settings.py
+++ src/_project/settings/core_settings.py
@@ -281,8 +281,7 @@ CELERY_BEAT_SCHEDULE = {
     "create_events_of_tournament_id1": {
         "task": "events.tasks.create_events_of_tournament_id1",
         "schedule": crontab(
-            hour="4",
-            day_of_week="mon"
+            hour="*/1",
 
         ),
     },
--- src/events/tasks.py
+++ src/events/tasks.py
@@ -225,7 +225,11 @@ def create_events_of_tournament(rubric_id):
 
 @shared_task
 def create_events_of_tournament_id1():
-    return create_events_of_tournament(1)
+   ids = [1, 2, 3, 4, 6, 7, 12, 13, 15, 21, 25, 36]
+   for item in ids:
+       create_events_of_tournament(item)
+   return {"response": "create_events_of_tournament successfully"}
+
 
 
 @shared_task
