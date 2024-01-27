70aa65ed19043638818d67b87d554c56c3274642
jexterone <allertartur@mail.ru>
2024-01-27 19:01:53 +0400

    Россия турниры

--- src/_project/settings/core_settings.py
+++ src/_project/settings/core_settings.py
@@ -281,7 +281,8 @@ CELERY_BEAT_SCHEDULE = {
     "create_events_of_tournament_id1": {
         "task": "events.tasks.create_events_of_tournament_id1",
         "schedule": crontab(
-            hour="*/1",
+            hour="4",
+            day_of_week="mon"
         ),
     },
     "create_events_of_tournament_id2": {
@@ -471,8 +472,7 @@ CELERY_BEAT_SCHEDULE = {
     "create_tournament": {
         "task": "events.tasks.create_tournament",
         "schedule": crontab(
-            hour="4",
-            day_of_week="sun",
+            hour="*/1",
         ),
     },
     # "get_match_stream_link_football": {
