a9ebc8cdfe77d4708a4a3b62127c3289b06245a1
jexterone <allertartur@mail.ru>
2024-01-28 16:50:31 +0400

    debug toolbar

--- src/_project/settings/core_settings.py
+++ src/_project/settings/core_settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     # 'mainapp.blocked_user_middleware.BlockedUserMiddleware',
     # 'mainapp.middleware.BlockIPMiddleware',
     # 'mainapp.middleware.UserSessionMiddleware',
