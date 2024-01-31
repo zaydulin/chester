f0c5d303655e5b4220977127c0fe4a28515c9ccf
jexterone <allertartur@mail.ru>
2024-01-31 19:34:09 +0400

    createtrnmt

--- src/_project/settings/core_settings.py
+++ src/_project/settings/core_settings.py
@@ -283,8 +283,9 @@ CELERY_BEAT_SCHEDULE = {
     "create_events_of_tournament_id1": {
         "task": "events.tasks.create_events_of_tournament_id1",
         "schedule": crontab(
-            hour="4",
-            day_of_week="mon"
+            hour="*/1",
+           # day_of_week="mon"
+
         ),
     },
     "create_events_of_tournament_id2": {
@@ -474,8 +475,8 @@ CELERY_BEAT_SCHEDULE = {
     "create_tournament": {
         "task": "events.tasks.create_tournament",
         "schedule": crontab(
-            hour="*/1"
-            #day_of_week="sun"
+            hour="3",
+            day_of_week="sun"
         ),
     },
     # "get_match_stream_link_football": {
