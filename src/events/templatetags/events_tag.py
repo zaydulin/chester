ec010cf231e3235aa1a6c53781a4ca7724eb0049
jexterone <allertartur@mail.ru>
2024-02-01 12:52:18 +0400

    template tag fix

--- src/events/templatetags/events_tag.py
+++ src/events/templatetags/events_tag.py
@@ -119,11 +119,11 @@ def get_country_seasons_popular(request):
         seasons = Season.objects.filter(country__isnull=False,sort_by__isnull = False,rubrics__api_id=1).order_by("country")
     sorted_seasons = sorted(seasons, key=lambda season: season.country.sort_by if season.country and season.country.sort_by is not None else float('inf'))
 
-    grouped_seasons = {}
+    grouped_seasons_popular = {}
     for country, season_group in groupby(sorted_seasons, key=lambda season: season.country):
-        grouped_seasons[country] = list(season_group)
+        grouped_seasons_popular[country] = list(season_group)
 
-    return grouped_seasons
+    return grouped_seasons_popular
 
 @register.simple_tag()
 def get_context(request):
