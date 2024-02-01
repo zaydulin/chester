22e4d86be9ebabf35921f41e6a326003d45479da
jexterone <allertartur@mail.ru>
2024-02-01 12:23:38 +0400

    migrate file

--- src/events/models.py
+++ src/events/models.py
@@ -295,7 +295,7 @@ class Country(models.Model):
     name = models.CharField(max_length=150)
     name_en = models.CharField(max_length=150)
     image = models.CharField(verbose_name='Флаг в формате flag-icon flag-icon-<название>',max_length=150)
-    sort_by = models.PositiveIntegerField('Позиция в списке')
+    sort_by = models.PositiveIntegerField('Позиция в списке', blank=True, null=True)
     def __str__(self):
         return f'{self.name}'
 
