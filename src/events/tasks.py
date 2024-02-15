54f15294d84f673783d8693eb823e810d5cd7c72
jexterone <allertartur@mail.ru>
2024-02-15 15:48:09 +0400

    поправил расписание тасков

--- src/events/tasks.py
+++ src/events/tasks.py
@@ -324,90 +324,71 @@ def fetch_event_data(rubric_id):
     )
     url = "https://flashlive-sports.p.rapidapi.com/v1/events/data"
     for event in events:
-        retries = 0
-        while retries < MAX_RETRIES:
-            querystring = {"locale": "en_INT", "event_id": event.second_event_api_id}
-            response = requests.get(url, headers=HEADER_FOR_SECOND_API, params=querystring)
-            if response.status_code == 200:
-                response_data = response.json().get("DATA").get("EVENT")
-                event.home_score = response_data.get("HOME_SCORE_CURRENT")
-                event.away_score = response_data.get("AWAY_SCORE_CURRENT")
-                event.status = EVENT_STATUSES[response_data.get("STAGE_TYPE")]
-                event.save()
-                break
-            elif response.status_code == 429:
-                retries += 1
-                if retries < MAX_RETRIES:
-                    if retries == MAX_RETRIES:
-                        return {"response": f"Error fetch - {response.status_code} - {response.json()}"}
-                    time.sleep(1)
-                    continue
-                else:
-                    return {"response": f"Error fetch - {response.status_code} - {response.json()}"}
+        querystring = {"locale": "en_INT", "event_id": event.second_event_api_id}
+        response = requests.get(url, headers=HEADER_FOR_SECOND_API, params=querystring)
+        if response.status_code == 200:
+            response_data = response.json().get("DATA").get("EVENT")
+            event.home_score = response_data.get("HOME_SCORE_CURRENT")
+            event.away_score = response_data.get("AWAY_SCORE_CURRENT")
+            event.status = EVENT_STATUSES[response_data.get("STAGE_TYPE")]
+            event.save()
+        else:
+            return {"response": f"Error fetch - {response.status_code} - {response.json()}"}
     # incidents
     for event in incidents_events:
-
         incidents_response = requests.get(
             incidents_url,
             headers=HEADER_FOR_SECOND_API,
             params={"locale": "ru_RU", "event_id": event.second_event_api_id}
         )
-        retries = 0
-        while retries < MAX_RETRIES:
-            if incidents_response.status_code == 200:
-                incidents_response_data = incidents_response.json().get("DATA", [])
-                for data in incidents_response_data:
-                    fields = {
-                        "home_score": data.get('RESULT_HOME', 0),
-                        "away_score": data.get('RESULT_AWAY', 0)
-                    }
-                    period, created = Periods.objects.get_or_create(
-                        period_number=data.get('STAGE_NAME', 0),
-                        event_api_id=event.second_event_api_id,
-                        defaults=fields
+
+        if incidents_response.status_code == 200:
+            incidents_response_data = incidents_response.json().get("DATA", [])
+            for data in incidents_response_data:
+                fields = {
+                    "home_score": data.get('RESULT_HOME', 0),
+                    "away_score": data.get('RESULT_AWAY', 0)
+                }
+                period, created = Periods.objects.get_or_create(
+                    period_number=data.get('STAGE_NAME', 0),
+                    event_api_id=event.second_event_api_id,
+                    defaults=fields
+                )
+                if not created:
+                    for field, value in fields.items():
+                        if getattr(period, field) != value:
+                            setattr(period, field, value)
+                period.save()
+                event.save()
+                event.periods.add(period)
+                data_items = data.get("ITEMS", [])
+                for item in data_items:
+                    incident, created = event.incidents.get_or_create(
+                        incident_api_id=item.get('INCIDENT_ID'),
+                        rubrics=rubrics,
+                        defaults={
+                            "incident_team": item.get('INCIDENT_TEAM'),
+                            "time": item.get('INCIDENT_TIME', '0')
+                        }
                     )
-                    if not created:
-                        for field, value in fields.items():
-                            if getattr(period, field) != value:
-                                setattr(period, field, value)
-                    period.save()
-                    event.save()
-                    event.periods.add(period)
-                    data_items = data.get("ITEMS", [])
-                    for item in data_items:
-                        incident, created = event.incidents.get_or_create(
-                            incident_api_id=item.get('INCIDENT_ID'),
-                            rubrics=rubrics,
+                    incident_participants_objs = []
+                    for participant in item.get('INCIDENT_PARTICIPANTS', []):
+                        incident_participant, created = IncidentParticipants.objects.get_or_create(
+                            participant_id=participant.get("PARTICIPANT_ID"),
                             defaults={
-                                "incident_team": item.get('INCIDENT_TEAM'),
-                                "time": item.get('INCIDENT_TIME', '0')
+                                "incident_type": participant.get("INCIDENT_TYPE"),
+                                "participant_name": participant.get("PARTICIPANT_NAME"),
+                                "incident_name": participant.get("PARTICIPANT_NAME"),
                             }
                         )
-                        incident_participants_objs = []
-                        for participant in item.get('INCIDENT_PARTICIPANTS', []):
-                            incident_participant, created = IncidentParticipants.objects.get_or_create(
-                                participant_id=participant.get("PARTICIPANT_ID"),
-                                defaults={
-                                    "incident_type": participant.get("INCIDENT_TYPE"),
-                                    "participant_name": participant.get("PARTICIPANT_NAME"),
-                                    "incident_name": participant.get("PARTICIPANT_NAME"),
-                                }
-                            )
-                            incident_participants_objs.append(incident_participant)
-                        incident.incident_participants.add(*incident_participants_objs)
-                        incident.save()
-                        event.incidents.add(incident)
-                        event.save()
-                break
-            elif incidents_response.status_code == 429:
-                retries += 1
-                if retries < MAX_RETRIES:
-                    if retries == MAX_RETRIES:
-                        return {"response": f"Error fetch - {incidents_response.status_code} - {incidents_response.json()}"}
-                    time.sleep(1)
-                    continue
-                else:
-                    return {"response": f"Error fetch - {incidents_response.status_code} - {incidents_response.json()}"}
+                        incident_participants_objs.append(incident_participant)
+                    incident.incident_participants.add(*incident_participants_objs)
+                    incident.save()
+                    event.incidents.add(incident)
+                    event.save()
+
+        else:
+            return {"response": f"Error fetch - {incidents_response.status_code} - {incidents_response.json()}"}
     # gamestatistic
     for event in gamestatistic_events:
         gamestatistic_response = requests.get(
@@ -417,49 +398,38 @@ def fetch_event_data(rubric_id):
                 "locale": "ru_RU",
                 "event_id": event.second_event_api_id}
         )
-        retries = 0
-        while retries < MAX_RETRIES:
-            if gamestatistic_response.status_code == 200:
-                gamestatistic_response_data = gamestatistic_response.json().get("DATA", [])
-                for data in gamestatistic_response_data:
-                    stage_name = data.get("STAGE_NAME", 0)
-                    groups = data.get("GROUPS")
-                    for group in groups:
-                        items = group.get("ITEMS", [])
-                        for item in items:
-                            incident_name = item.get("INCIDENT_NAME")
-                            value_home = item.get("VALUE_HOME")
-                            value_away = item.get("VALUE_AWAY")
-                            if event.statistic.filter(name=incident_name).exists():
-                                gamestatistic = event.statistic.filter(name=incident_name).first()
-                                gamestatistic.period = stage_name
-                                gamestatistic.home = value_home
-                                gamestatistic.away = value_away
-                                gamestatistic.save()
+        if gamestatistic_response.status_code == 200:
+            gamestatistic_response_data = gamestatistic_response.json().get("DATA", [])
+            for data in gamestatistic_response_data:
+                stage_name = data.get("STAGE_NAME", 0)
+                groups = data.get("GROUPS")
+                for group in groups:
+                    items = group.get("ITEMS", [])
+                    for item in items:
+                        incident_name = item.get("INCIDENT_NAME")
+                        value_home = item.get("VALUE_HOME")
+                        value_away = item.get("VALUE_AWAY")
+                        if event.statistic.filter(name=incident_name).exists():
+                            gamestatistic = event.statistic.filter(name=incident_name).first()
+                            gamestatistic.period = stage_name
+                            gamestatistic.home = value_home
+                            gamestatistic.away = value_away
+                            gamestatistic.save()
 
-                            if event.statistic.filter(period=stage_name, name=incident_name, home=value_home,
-                                                      away=value_away).exists():
-                                pass
-                            else:
-                                gamestatistic = GameStatistic.objects.create(
-                                    period=stage_name,
-                                    name=incident_name,
-                                    home=value_home,
-                                    away=value_away
-                                )
-                                event.statistic.add(gamestatistic)
-                            event.save()
-                break
-            elif gamestatistic_response.status_code == 429:
-                retries += 1
-                if retries < MAX_RETRIES:
-                    if retries == MAX_RETRIES:
-                        return {
-                            "response": f"Error fetch - {gamestatistic_response.status_code} - {gamestatistic_response.json()}"}
-                    time.sleep(1)
-                    continue
-                else:
-                    return {"response": f"Error fetch - {gamestatistic_response.status_code} - {gamestatistic_response.json()}"}
+                        if event.statistic.filter(period=stage_name, name=incident_name, home=value_home,
+                                                  away=value_away).exists():
+                            pass
+                        else:
+                            gamestatistic = GameStatistic.objects.create(
+                                period=stage_name,
+                                name=incident_name,
+                                home=value_home,
+                                away=value_away
+                            )
+                            event.statistic.add(gamestatistic)
+                        event.save()
+        elif gamestatistic_response.status_code == 429:
+            return {"response": f"Error fetch - {gamestatistic_response.status_code} - {gamestatistic_response.json()}"}
     return {"response": f"fetch_event_data_for_second successfully"}
 
 #события на затрва (нужны тк таска отправляет запрос но не получает некоторые матчи ,проблема апи)
@@ -552,17 +522,12 @@ def get_events_tommorow(sport_id):
                                                              datetime.utcfromtimestamp(event.get("START_TIME"))),
                                 ))
                     Events.objects.bulk_create(events_list)
-                break
-            elif response.status_code == 429:
-                retries += 1
-                if retries < MAX_RETRIES:
-                    if retries == MAX_RETRIES:
-                        return {
-                            "response": f"Error fetch - {response.status_code} - {response.json()}"}
-                    time.sleep(3)
-                    continue
-                else:
-                    return {"response": f"Error fetch - {response.status_code} - {response.json()}"}
+
+            else:
+                return {"response": f"Error fetch - {response.status_code} - {response.json()}"}
+
+    return {"response": f"get_events_tommorow ok"}
+
 @shared_task
 def get_events_tommorow_id1():
     return get_events_tommorow(1)
