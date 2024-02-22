71eb903363711f089639895871736b1222558ae7
jexterone <allertartur@mail.ru>
2024-02-22 20:53:47 +0400

    timer fix

--- src/events/tasks.py
+++ src/events/tasks.py
@@ -577,7 +577,7 @@ def update_event_data(sport_id):
                             existing_event.status = status_id
                             existing_event.start_at = datetime.utcfromtimestamp(event.get("START_TIME"))
                             start_time_fact = event.get("STAGE_START_TIME")
-                            if start_time_fact:
+                            if start_time_fact and not existing_event.start_at_for_timer:
                                 existing_event.start_at_for_timer = datetime.utcfromtimestamp(start_time_fact)
                             existing_event.half = event.get("ROUND")
                             events_list_update.append(existing_event)
